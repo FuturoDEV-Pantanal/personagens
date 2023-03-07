1df944e5672a74bf936d2e39cdb3ff7a5985e339
tiagoamp <tiagoamp@gmail.com>
2023-03-07 20:06:20 -0300

    WIP: Componente Lista Personagens

---
+++ src/components/BlocoPersonagem/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+export default function BlocoPersonagem() {
+  return (
+    <div>BlocoPersonagem</div>
+  )
+}
--- src/components/ListaPersonagens/index.js
+++ src/components/ListaPersonagens/index.js
@@ -1,15 +1,17 @@
+import BlocoPersonagem from '../BlocoPersonagem';
 import './styles.css';
 
 export default function ListaPersonagens( { personagens } ) {
-
-    // console.log('dentro do componente de listagem: ', personagens);
 
   return (
     <section className='personagens-container'>
         <div className='lista-titulo'>Personagens</div>
         <div className='lista-dados'>
-            lista dos personagens
+            {
+                personagens.map(pers => <BlocoPersonagem />)
+            }
         </div>        
     </section>
   )
 }
+
