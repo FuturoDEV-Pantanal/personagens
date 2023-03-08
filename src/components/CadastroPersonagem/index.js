b1a3c72a259d2d79e7bd0cdfee28d235ad7c6537
tiagoamp <tiagoamp@gmail.com>
2023-03-07 21:41:54 -0300

    Implementacao componente cadastro de personagem

--- src/components/CadastroPersonagem/index.js
+++ src/components/CadastroPersonagem/index.js
@@ -7,15 +7,15 @@ export default function CadastroPersonagem() {
         <form className="form-personagem">
           <div className='form-grupo'>
             <label htmlFor="input-nome">Nome: </label>
-            <input type="text" required id="input-nome" size={50} placeholder="Informe o nome" />
+            <input type="text" required id="input-nome" placeholder="Informe o nome" />
           </div>
           <div className='form-grupo'>
             <label htmlFor="input-serie">Série: </label>
-            <input type="text" required id="input-serie" size={50} placeholder="Informe a Série" />
+            <input type="text" required id="input-serie" placeholder="Informe a Série" />
           </div>
           <div className='form-grupo'>
             <label htmlFor="input-imagem">URL da Imagem: </label>
-            <input type="text" id="input-imagem" size={50} placeholder="http://www..." />
+            <input type="text" id="input-imagem" placeholder="http://www..." />
           </div>
             <input type="submit" value="Adicionar" className='btn-adicionar' /> 
         </form>
