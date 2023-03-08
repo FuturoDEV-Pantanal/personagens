a27846ad180501421e083b0099710063fa112a9c
tiagoamp <tiagoamp@gmail.com>
2023-03-07 21:21:32 -0300

    WIP: Componente Lista Personagens

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ function App() {
   useEffect( () => {
     fetch('dados.json')
       .then(resp => resp.json())
+      .then(dados => new Promise(resolve => setTimeout(() => resolve(dados), 5000)))
       .then(dados => setPersonagens(dados))
       .catch(erro => console.log(erro));
   }, []);
   
+  if (personagens.length === 0) {  // Se array de personagens vazio
+    return (<h1>Aguarde...</h1>);
+  }
+
   return (
     <div className="App">
       
